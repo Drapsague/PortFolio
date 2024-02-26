6a96e27333c9ef32055216a5889b0a831155ed4e
Drapsague <gaspard.couly@proton.me>
2024-02-26 23:31:33 +0100

    Opti Commmit

--- script.js
+++ script.js
@@ -18,38 +18,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-
-/*
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let interval = null;
-
-document.querySelectorAll('.menu').forEach(menuItem => {
-    menuItem.onmouseover = (event) => {
-        let iteration = 0;
-        clearInterval(interval);
-        interval = setInterval(() => {
-            event.target.innerText = event.target.innerText
-            .split("")
-            .map((letter, index) => {
-                if(index < iteration) {
-                    return event.target.dataset.value[index];
-                }
-            
-                return letters[Math.floor(Math.random() * 26)]
-            })
-            .join("");
-            
-            if(iteration >= event.target.dataset.value.length){ 
-                clearInterval(interval);
-            }
-            
-            iteration += 1/1.8 ;
-        }, 30);
-    }
-   
-});
-*/
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelectorAll('.menu').forEach(menuItem => {
@@ -106,14 +74,14 @@ function updateMenuOnScroll() {
         const boxIcon = link.querySelector('box-icon');
         
         if (menuItemP.dataset.section === `#${closestSectionId}`) {
-            menuItemP.style.color = 'white'; // Mettez en surbrillance l'élément du menu
-            boxIcon.setAttribute('type', 'solid'); // Changez le type de l'icône en 'solid'
+            menuItemP.style.color = 'white'; 
+            boxIcon.setAttribute('type', 'solid');
             boxIcon.setAttribute(`color`, `white`);
             //section2.classList.add('in-view');
         } else {
             
-            menuItemP.style.color = "#94a3b8"; // Couleur par défaut
-            boxIcon.setAttribute('type', 'regular'); // Réinitialisez le type de l'icône en 'regular'
+            menuItemP.style.color = "#94a3b8"; 
+            boxIcon.setAttribute('type', 'regular'); 
             boxIcon.setAttribute(`color`, `#94a3b8`);
             //section2.classList.remove('in-view');
         }
@@ -121,205 +89,4 @@ function updateMenuOnScroll() {
 }
 
 window.addEventListener('scroll', updateMenuOnScroll);
-updateMenuOnScroll(); // Initialiser au chargement de la page
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-document.querySelectorAll('.meny').forEach(function(eventClicked) {
-    eventClicked.addEventListener('click', function() {
-    
-        document.querySelectorAll('.meny box-icon').forEach(function(icon) {
-            icon.setAttribute( `type`, `regular`); 
-            icon.setAttribute('color', '#71717a'); 
-        });
-        document.querySelectorAll('.meny p').forEach(function(menuItem) {
-            menuItem.style.color = "#71717a"; // Couleur par défaut
-        });
-
-        let typeElement = eventClicked.querySelector('box-icon');
-        let menuElement = eventClicked.querySelector('p');
-        typeElement.setAttribute('type', 'solid'); 
-        typeElement.setAttribute('color', 'white'); 
-        menuElement.style.color = "white"; 
-    });
-});
-
-
-
-
-
-
-
-function updateMenuOnScroll() {
-    const sections = document.querySelectorAll('section');
-    let closestSectionId = null;
-    let closestSectionDistance = Infinity; // Distance du haut du viewport
-
-    sections.forEach(section => {
-        const rect = section.getBoundingClientRect();
-
-        // Vérifiez si la section est partiellement visible
-        if (rect.bottom > 0 && rect.top < window.innerHeight) {
-            // Vérifiez si cette section est plus proche du haut du viewport que les précédentes vérifiées
-            if (rect.top < closestSectionDistance && rect.top >= 0) {
-                closestSectionDistance = rect.top;
-                closestSectionId = section.id;
-            }
-        }
-    });
-
-    // Mise à jour des éléments du menu
-    document.querySelectorAll('.menu').forEach(menuItem => {
-        var menuItemP = menuItem.querySelector('p');
-        menuItem.style.color = "#71717a"; // Réinitialisez la couleur de tous les éléments du menu
-        //menuItemP.setAttribute(`type`, `regular`);
-
-        if (menuItem.dataset.section === `#${closestSectionId}`) {
-            //menuItemP.style.color = 'white'; // Mettez en surbrillance l'élément du menu correspondant
-            menuItem.style.color = 'white';
-            //menuItemP.setAttribute(`type`, `solid`);
-        }
-    });
-}
-
-window.addEventListener('scroll', updateMenuOnScroll);
-updateMenuOnScroll(); // Initialiser au chargement de la page
-
-
-
-// Fonction pour mettre à jour le menu en fonction de la section visible
-function updateMenuOnScroll() {
-    const sections = document.querySelectorAll('section'); // Sélectionnez toutes les sections de la page
-  
-    // Pour chaque section, vérifiez si elle est visible à l'écran
-    sections.forEach(section => {
-      const rect = section.getBoundingClientRect(); // Récupérez les dimensions de la section
-        
-      // Si la section est visible à l'écran (au moins partiellement), mettez à jour le menu
-      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-        const sectionId = section.id; // Obtenez l'identifiant de la section visible
-        const menuItem = document.querySelector(`[data-section="#${sectionId}"]`); // Trouvez l'élément du menu correspondant
-  
-        // Réinitialisez la couleur de tous les éléments du menu
-       
-        document.querySelectorAll('.meny p').forEach(function(menuItem) {
-            menuItem.style.color = "#71717a"; // Couleur par défaut
-        });
-  
-        // Mettez en surbrillance l'élément du menu correspondant à la section visible
-        menuItem.style.color = 'white';
-      }
-    });
-  }
-  
-  // Écoutez l'événement de défilement de la page
-  window.addEventListener('scroll', updateMenuOnScroll);
-  
-  // Appelez la fonction une fois au chargement de la page pour initialiser le menu
-  updateMenuOnScroll();
-
-
-
-
-
-
-
-
-document.querySelectorAll('.meny').forEach(function(eventclicked){
-    eventclicked.addEventListener('click', function(){
-        let typeElement = eventclicked.querySelector(`box-icon`);
-        let menuElement = eventclicked.querySelector(`p`);
-        let typeValue = typeElement.getAttribute(`type`);
-        if(typeValue === `solid`){
-            console.log('test');
-        }else {
-            
-            typeElement.setAttribute(`type`,`solid`);
-            typeElement.setAttribute(`color`, `white`);
-            menuElement.style.color = "white";
-
-        }
-       
-    })
-})
-
-
-
-
-
-
-
-
-document.querySelectorAll('.menu').forEach(menuItem => {
-    menuItem.onmouseover = (event) => {
-        let iteration = 0;
-  
-        clearInterval(interval);
-        
-        interval = setInterval(() => {
-            event.target.innerText = event.target.innerText
-            .split("")
-            .map((letter, index) => {
-                if(index < iteration) {
-                    return event.target.dataset.value[index];
-                }
-            
-                return letters[Math.floor(Math.random() * 26)]
-            })
-            .join("");
-            
-            if(iteration >= event.target.dataset.value.length){ 
-                clearInterval(interval);
-            }
-            
-            iteration += 1 / 2.5;
-        }, 30);
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.querySelectorAll('.menu').forEach(function(element){
-    element.addEventListener('click', function(event){
-        var caseClicked = event.target;
-        var welcome = caseClicked.classList.contains('welcome');
-        if (welcome){
-            console.log("test");
-        }
-
-
-
-        caseClicked.innerHTML = `&lt;/WELCOME&gt;`;
-        caseClicked.classList.add('active');
-        
-    });
-});
-
-
-*/
-
-
-
+updateMenuOnScroll(); 
